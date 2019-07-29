c77d581eecf75fcdf240f0e12777cf4a379c21a2
Matt Espe <mespe@ucdavis.edu>
2019-07-29 13:07:41 -0600

    Rearranging model functions to reduce redundency.

--- R/model_funs.R
+++ R/model_funs.R
@@ -1,34 +1,55 @@
 eDNA_lm = function(formula, data, fit_fun = rstan::sampling,
                    std_curve_alpha, std_curve_beta,
                    upper_Cq = 40,
-                   n_chain = 1L, iters = 500L, verbose = FALSE, ...)
+                   n_chain = 1L, iters = 1000L, verbose = FALSE, ...)
 {
     
-    ml = if(!is_lme4(formula)) {
-             gen_model_list_lm(formula, data)
-         } else {
-             gen_model_list_lmer(formula, data)
-         }
-
+    ml = gen_model_list_lm(formula, data)
+       
     # This works because Stan ignores extra input data
     md = prep_model(ml, std_curve_alpha, std_curve_beta,
                     Cq_upper = upper_Cq)
 
     md$y = ml$y
+    fit = run_model(data = md, n_chain = n_chain, iters = iters, verbose = verbose, ...)
+    return(fit)
+}
+
+eDNA_lmer = function(formula, data, 
+                   std_curve_alpha, std_curve_beta,
+                   upper_Cq = 40,
+                   n_chain = 1L, iters = 1000L, verbose = FALSE, ...)
+{
+    
+    ml = gen_model_list_lmer(formula, data)
+    
+    md = prep_model(ml, std_curve_alpha, std_curve_beta,
+                    Cq_upper = upper_Cq)
+
+    md$y = ml$y
+    fit = run_model(data = md, n_chain = n_chain, iters = iters, verbose = verbose, ...)
+    return(fit)
+}
+
+run_model = function(model = stanmodels$eDNA_lm, data, n_chain,
+                  iters, verbose, ...)
+
+{
     if(!verbose) sink(tempfile())
     
-    fit = fit_fun(stanmodels$eDNA_lm, data = md, chains = n_chain,
-                  algorithm = "Fixed_param", iter = iters,
-                  refresh = ifelse(verbose, 100, -1), show_messages = verbose)
+    fit = sampling(model, data, chains = n_chain,
+                  iter = iters,
+                  refresh = ifelse(verbose, 100, -1), show_messages = verbose, ...)
 
     if(!verbose) sink()
     
     cl = if(is_lme4(formula)) "eDNA_model_lmer" else "eDNA_model_lm"
     
-    fit = as(fit, cl)
-    return(fit)
-}
+    fit = as(fit, "eDNA_model")
+    class(fit) = cl
 
+
+}
 
 prep_model = function(mod_list, alpha, beta, Cq_upper = 40)
 {
