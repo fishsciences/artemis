4bd5ae2c9de2beae93a3471e9cfc0ef10b6d7964
Matt Espe <mespe@ucdavis.edu>
2019-07-30 16:59:34 -0600

    Fix print to use summary.

--- R/print.R
+++ R/print.R
@@ -1,18 +1,26 @@
-setMethod("print", "eDNA_simulation",
-          function(x, FUN = summary, digits = 3) {
-              cat("\nformula: "); print(x@formula)
-              cat("\nvariable levels:\n")
-              
-              vars = sapply(seq_along(x@variable_levels), function(i)
-                  paste0(names(x@variable_levels)[i],  " :",
-                         paste(x@variable_levels[[i]], collapse = " ")))
+print.eDNA_simulation = function(x, FUN = summary, digits = getOption("digits")) {
+    cat("\nformula: "); print(x@formula)
+    cat("\nvariable levels:\n")
+    
+    vars = sapply(seq_along(x@variable_levels), function(i)
+        paste0(names(x@variable_levels)[i],  " :",
+               paste(x@variable_levels[[i]], collapse = " ")))
+    
+    cat(vars, sep = "\n")
+    cat("\n ln concentration: \n")
+    print(summary(x, var = "ln_conc", digits = digits))
 
-              cat(vars, sep = "\n")
-              cat("\n ln concentration: \n")
-              print(get_marginals(x@ln_conc, x@x, FUN, digits))
+    cat("\n simulated Cq: \n")
+    print(summary(x, digits = digits))    
+    return(invisible(NULL))
+}
 
-              cat("\n simulated Cq: \n")
-              print(get_marginals(x@Cq_star, x@x, FUN, digits))
-              
-              return(invisible(NULL))
-          })
+print.eDNA_simulation.summary = function(x, digits = getOption("digits"), ...)
+{
+    x = as.data.frame(sapply(x,
+                             function(y, digs)
+                                 if(is.numeric(y)) round(y, digs) else y
+                           , digs = digits),
+                      stringsAsFactors = FALSE)
+    NextMethod()
+}
