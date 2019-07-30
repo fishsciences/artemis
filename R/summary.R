89cacadf63d096fba1fcc12e9ae9fdae304271ba
Matt Espe <mespe@ucdavis.edu>
2019-07-30 16:46:02 -0600

    Working out summary functions using S3

--- R/summary.R
+++ R/summary.R
@@ -1,8 +1,39 @@
+# S3 methods
+summary.eDNA_simulation = function(object, var = "Cq_star",
+                                   probs = c(0.025, 0.5, 0.975), ...)
+{
+    ## Super ugly - clean up later
+    qtl = get_marginals(slot(object, var), object@x, quantile, probs = probs)
+    qtl = lapply(seq_along(qtl), function(i) {
+        tmp = qtl[i]
+        tmp = as.data.frame(tmp, stringsAsFactors = FALSE)
+        colnames(tmp) = paste0(probs * 100, "%")
+        tmp = cbind(variable = names(qtl)[i],
+                    level = as.numeric(rownames(qtl[[i]])),
+                    tmp, stringsAsFactors = FALSE)
+        tmp
+    })
+    qtl = do.call(rbind, qtl)
+    m = get_marginals(slot(object, var), object@x, mean)
+    
+    dt = if(var == "Cq_star"){
+             get_marginals(slot(object, var), object@x, p_detect,
+                           thresh = object@upper_Cq)
+         } else NA
+    
+    ans = cbind(qtl, mean = unlist(m), p_detect = unlist(dt))
+    structure(ans,
+              class = c("eDNA_simulation.summary", "data.frame"))
+    
+}
 
-setMethod("summary", "eDNA_simulation",
-          function(object, ...) {
-              qtl = get_marginals(object@Cq_star, object@x, quantile)
-              dt = get_marginals(object@Cq_star, object@x, p_detect,
-                                 thresh = object@upper_Cq)
-              mapply(function(a, b) cbind(a,p_detect = b), qtl, dt)
-          })
+summary.eDNA_model = function(object, probs = c(0.025, 0.5, 0.975), ...)
+{
+    res = t(apply(object@betas, 2, quantile, prob = probs))
+    res = as.data.frame(res)
+    rownames(res) = colnames(object@x)
+    res$mean = colMeans(object@betas)
+    
+    structure(res,
+              class = c("eDNA_model.summary", "data.frame"))
+}
---
+++ tests/testthat/test_summary.R
@@ -0,0 +1,29 @@
+context("Simulating data")
+
+vars = list(distance = c(0, 15, 50),
+            volume = c(25, 50),
+            biomass = 100,
+            alive = 1,
+            tech_rep = 1:10,
+            rep = 1:3, Cq = 1)
+
+X = expand.grid(vars)
+betas = c(distance = .002, volume = -0.57, biomass = 1, alive = 1)
+
+test_that("Summary methods", {
+    ans = sim_eDNA_lm(Cq ~ distance + volume, vars,
+                      betas = c(intercept = 1, distance = 0.5, volume = 0),
+                      sigma_Cq = 1, std_curve_alpha = 21.2, std_curve_beta = -1.5)
+
+    res = summary(ans)
+
+    expect_is(res, "eDNA_simulation.summary")
+    expect_is(res, "data.frame")
+
+    expect_true(nrow(res) == sum(sapply(ans@x, function(x) length(unique(x)))))
+    expect_true(ncol(res) == 7)
+    
+    res2 = summary(ans, prob = 0.5)
+
+    expect_true(ncol(res2) == 5)
+})
