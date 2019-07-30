0aae74216ded339df6cc723943571752df520d09
Matt Espe <mespe@ucdavis.edu>
2019-07-30 12:16:57 -0600

    Skeleton of plot functions.

---
+++ R/plot.R
@@ -0,0 +1,12 @@
+setMethod("plot", "eDNA_simulation",
+          function(x, y, ...) {})
+
+setMethod("plot", "eDNA_model",
+          function(x, y, ...) {})
+
+## Might do something slightly different/expanded with lmer sims and fits
+setMethod("plot", "eDNA_simulation_lmer",
+          function(x, y, ...) {})
+
+setMethod("plot", "eDNA_model_lmer",
+          function(x, y, ...) {})
