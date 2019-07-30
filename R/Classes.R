79eb67400c2746405ffd6e8781cd124ac7d2f178
Matt Espe <mespe@ucdavis.edu>
2019-07-30 12:19:34 -0600

    Moving print and summary to own scripts.

--- R/Classes.R
+++ R/Classes.R
@@ -26,32 +26,6 @@ setAs("stanfit", "eDNA_simulation",
       })
 
 
-setMethod("print", "eDNA_simulation",
-          function(x, FUN = summary, digits = 3) {
-              cat("\nformula: "); print(x@formula)
-              cat("\nvariable levels:\n")
-              
-              vars = sapply(seq_along(x@variable_levels), function(i)
-                  paste0(names(x@variable_levels)[i],  " :",
-                         paste(x@variable_levels[[i]], collapse = " ")))
-
-              cat(vars, sep = "\n")
-              cat("\n ln concentration: \n")
-              print(get_marginals(x@ln_conc, x@x, FUN, digits))
-
-              cat("\n simulated Cq: \n")
-              print(get_marginals(x@Cq_star, x@x, FUN, digits))
-              
-              return(invisible(NULL))
-          })
-
-setMethod("summary", "eDNA_simulation",
-          function(object, ...) {
-              qtl = get_marginals(object@Cq_star, object@x, quantile)
-              dt = get_marginals(object@Cq_star, object@x, p_detect,
-                                 thresh = object@upper_Cq)
-              mapply(function(a, b) cbind(a,p_detect = b), qtl, dt)
-          })
 
 setAs("eDNA_simulation", "data.frame",
       function(from){
