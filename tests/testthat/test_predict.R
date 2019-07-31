2c6d3ae43e5065bcfa7d997168a98fa7d04b1480
Matt Espe <mespe@ucdavis.edu>
2019-07-31 11:50:10 -0600

    Update predict tests.

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -5,7 +5,19 @@ test_that("Model predictions",{
     ans = predict(dd)
     
     expect_is(ans, "eDNA_predict_lm")
+
+    ans2 = predict(dd, interval = TRUE)
+    expect_is(ans2, "eDNA_predict_lm")
+    expect_true(ncol(ans2$ln_conc) == 2)
+    expect_true(ncol(ans2$Cq_hat) == 2)
+    expect_true(is.null(ans$Cq_star))
+
+    ans3 = predict(dd, interval = TRUE, include_sigma = TRUE)
+    expect_true(!is.null(ans3$Cq_star))
+
+    x = data.frame(Intercept = 1, Distance = c(0,50))
  
-    ans = eDNA_lm(Cq ~ Distance, eDNA_samples, fit_fun = rstan::optimizing,
-                  std_curve_alpha = 21.2, std_curve_beta = -1.5)
+    ans4 = predict(dd, newdata = x)
+    
+    expect_true(nrow(ans4$ln_conc) == 2)
 })
