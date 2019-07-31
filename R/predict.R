92ab0e716056fd32e2f6c4f9390e5bef9fb20ce5
Matt Espe <mespe@ucdavis.edu>
2019-07-31 11:49:57 -0600

    Update predict method.

--- R/predict.R
+++ R/predict.R
@@ -1,32 +1,42 @@
-setClass("eDNA_predict_lm",
-         slots = c(ln_conc = "matrix",
-                   Cq_hat = "matrix"))
 
-setClass("eDNA_predict_lmer", contains = "eDNA_predict_lm")
+predict.eDNA_model = function(object, newdata = NULL, include_sigma = FALSE,
+                              interval = FALSE, ...)
+{
 
-setMethod("predict", "eDNA_model",
-          function(object, newdata = NULL, 
-                   type = c(), ...)
-          {
-              if(!is.null(newdata)){
-                  X = newdata
-              } else {
-                  X = object@x
-              }
-              
-              ln_conc = apply(object@betas, 1, function(x) as.matrix(X) %*% x)
-              Cq_hat = object@std_curve_alpha + object@std_curve_beta * ln_conc
-              new("eDNA_predict_lm",
-                  ln_conc = ln_conc,
-                  Cq_hat = Cq_hat)
-          })
+    
+    if(!is.null(newdata)){
+        if(ncol(x) != ncol(object@betas))
+            stop("Please provide the same number of predictors as the original data, including the intercept")
+        X = newdata
+    } else {
+        X = object@x
+    }
+    
+    ln_conc = apply(object@betas, 1, function(x) as.matrix(X) %*% x)
+    Cq_hat = object@std_curve_alpha + object@std_curve_beta * ln_conc
+    Cq_star = if(include_sigma) {
+                  sapply(seq(ncol(Cq_hat)), function(i)
+                      Cq_hat[,i] + rnorm(nrow(Cq_hat)) * object@sigma_Cq[i])
+    } else {
+        NULL
+    }
+    ans = list(ln_conc = ln_conc,
+               Cq_hat = Cq_hat,
+               Cq_star = Cq_star)
 
-setMethod("predict", "eDNA_model_lmer",
-          function(object, newdata = NULL, 
-                   type = c(), ...)
-          {
-              ans = callNextMethod()
-              ## Do something with the random effects here
-              
-              return(ans)
-          })
+    if(interval){
+        isNull = sapply(ans, is.null)
+        ans[!isNull] = lapply(ans[!isNull], posterior_interval, ...)
+    }
+    structure(ans,
+              class = "eDNA_predict_lm")
+}
+
+predict.eDNA_model_lmer = function(object, newdata = NULL, 
+                                   type = c(), ...)
+{
+    ans = NextMethod()
+    ## Do something with the random effects here
+    
+    return(ans)
+}
