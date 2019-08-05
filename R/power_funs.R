042769cd309429a28322198de5b2a33cbce25885
Matt Espe <mespe@ucdavis.edu>
2019-08-05 11:07:52 -0600

    Power functions - not 100% yet.

--- R/power_funs.R
+++ R/power_funs.R
@@ -1,5 +1,5 @@
 
-est_p_detect = function(var_levels,
+est_p_detect = function(variable_levels,
                         betas, 
                         Cq_sd,
                         std_curve_alpha, std_curve_beta,
@@ -9,15 +9,15 @@ est_p_detect = function(var_levels,
                         upper_Cq = 40)
 
 {
-    if(!is.null(dim(var_levels)))
+    if(!is.null(dim(variablelevels)))
         stop("Sorry, only one set of variable levels at a time currently supported")
-    if(is.null(model_fit) && length(var_levels) != length(betas))
+    if(is.null(model_fit) && length(variablelevels) != length(betas))
         stop("Variable levels and betas cannot be of different lengths")
 
     ln_conc_hat = if(is.null(model_fit)) {
-                      var_levels %*% betas
+                      variable_levels %*% betas
                   } else {
-                      apply(model_fit@betas, 1, function(y) var_levels %*% y)
+                      apply(model_fit@betas, 1, function(y) variable_levels %*% y)
                   }
 
     ln_thresh = (upper_Cq - std_curve_alpha) / std_curve_beta
@@ -25,7 +25,35 @@ est_p_detect = function(var_levels,
     ans = sapply(n_rep, function(i) 1 - (pnorm(ln_thresh, ln_conc_hat, Cq_sd) ^ i))
 
     structure(ans,
-              variable_levels = var_levels,
+              variable_levels = variable_levels,
               reps = n_rep,
               class = c("eDNA_p_detect", class(ans)))
+}
+
+est_power = function(formula, vars_list,
+                     betas, sigma_Cq,
+                     sigma_rep,
+                     std_curve_alpha, std_curve_beta,
+                     conf_level = 0.95,
+                     n_sim = 200L,
+                     upper_Cq = 40,
+                     X = expand.grid(vars_list),
+                     verbose = FALSE)
+{
+    
+    sims = sim_eDNA_lmer(formula, vars_list,
+                         betas, sigma_Cq,
+                         sigma_rep,
+                         std_curve_alpha, std_curve_beta,
+                         n_sim, upper_Cq,
+                         X, verbose)
+
+    fit = eDNA_lmer(formula,
+                    cbind(expand.grid(vars_list), Cq = sims@Cq_star[1,]),
+                    std_curve_alpha = std_curve_alpha,
+                    std_curve_beta = std_curve_beta,
+                    upper_Cq = upper_Cq, n_chain = 4, iters = 500, verbose = TRUE)
+
+
+
 }
