a18d9337c81b1b82801633b9970e8335feda7379
Matt Espe <mespe@ucdavis.edu>
2019-09-10 13:00:13 -0600

    Addresses #1.

--- R/summary.R
+++ R/summary.R
@@ -71,11 +71,12 @@ summary.eDNA_model = function(object, probs = c(0.025, 0.5, 0.975), ...)
         res = c(res, quantile(object@sigma_Cq, probs))
     }
     res = data.frame(res)
-
+    
     colnames(res) = paste0(probs * 100, "%")
     
     rownames(res) = c(colnames(object@x), "CQ sd")
-    res$mean = colMeans(cbind(object@betas, object@sigma_Cq))
+    res = cbind(mean = colMeans(cbind(object@betas, object@sigma_Cq)),
+                res)
     
     structure(res,
               iter = object@stanfit@stan_args$iter,
