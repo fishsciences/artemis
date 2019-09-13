bd184868709b97b691c3d4b20d7889411d845d5f
Matt Espe <mespe@ucdavis.edu>
2019-09-13 11:46:54 -0600

    Update plot functions to allow more args to be user adj.

--- R/plot.R
+++ R/plot.R
@@ -6,6 +6,8 @@
 ##' @param y ignored
 ##' @param response the response variable to plot
 ##' @param probs the probability for plotting CIs
+##' @param alpha the alpha value, i.e. transparancy, of the points
+##' @param jitter_width the width of the jitter applied to the points
 ##' @param ... ignored
 ##' @return a list of ggplots
 ##' @author Matt Espe
@@ -14,6 +16,7 @@
 plot.eDNA_simulation = function(x, y,
                                 response = "Cq_star",
                                 probs = c(0.025, 0.975),
+                                alpha = 0.1, jitter_width = 0.35, 
                                 ...)
 {
     y = slot(x, response)
@@ -27,7 +30,7 @@ plot.eDNA_simulation = function(x, y,
     vars = colnames(x@x)
     p = lapply(vars, function(v){
         ggplot(, aes(y = ymn, x = x@x[[v]] )) +
-            geom_jitter(alpha = 1/10, width = 0.35) +
+            geom_jitter(alpha = alpha, width = jitter_width) +
             xlab(v) +
             ylab(response) +
             theme_bw()
@@ -65,6 +68,8 @@ plot.eDNA_model = function(x, y, pars = "betas",  ...) {
 ##' @param probs numeric vector of length 2, the lower and upper probabilities for
 ##'     quantiles displayed
 ##' @param ylim the y limits on the plot
+##' @param point_size point size for the estimate or the mean value
+##' @param n_breaks passed to \code{pretty()}
 ##' @param ... additional args, currently ignored
 ##' @return a ggplot object
 ##' 
@@ -72,7 +77,10 @@ plot.eDNA_model = function(x, y, pars = "betas",  ...) {
 ##' @method plot eDNA_p_detect
 ##' @export
 plot.eDNA_p_detect = function(x, y, probs = c(0.025, 0.975),
-                              ylim = c(0,1), ...)
+                              ylim = c(0,1),
+                              point_size = 2.5,
+                              n_breaks = 5,
+                              ...)
 {
     if(length(probs) != 2)
         stop("Please provide a lower and upper bound in probs")
@@ -81,20 +89,22 @@ plot.eDNA_p_detect = function(x, y, probs = c(0.025, 0.975),
 
     if(is.matrix(x)) {
         tmp = summary(x, probs)
-        p = ggplot(tmp, aes(x = as.integer(n_rep), y = tmp$mean)) + 
-            geom_point(size = 2.5) +
-            geom_line(lty = 3) + 
-            geom_errorbar(aes(ymin = tmp[,3], # lower
-                              ymax = tmp[,4]), # upper
-                            width = rel(0.15)) 
+        p = ggplot(, aes(x = as.integer(tmp$n_rep), y = tmp$mean)) + #avoids R CMD check NOTE
+            geom_pointrange(aes(ymin = tmp[,3], # lower
+                                ymax = tmp[,4]), # upper
+                            size = point_size) +
+            geom_line(lty = 3) 
     } else {
         p = ggplot( , aes(x = as.integer(reps), y = x)) +
-            geom_point(size = 2.5) 
+            geom_point(size = point_size) 
     }
         p + ylab("p(detect)") + xlab("N replicates") +
-            scale_x_continuous(breaks = function(x, n = 5)  
-                pretty(x, n)[round(pretty(x, n),1) %% 1 == 0],
+            scale_x_continuous(breaks = break_fun(as.integer(reps) , n = n_breaks),
                 expand = expand_scale(mult = 0.015)) +
             scale_y_continuous(limits = ylim, expand = expand_scale(add = 0.01)) +
             theme_bw()
+}
+
+break_fun = function(x, n) {
+    pretty(x, n)[round(pretty(x, n),1) %% 1 == 0]
 }
