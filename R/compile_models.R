4867cc79d0daaec56c7d647e410863f59f2d66ce
Matt Espe <mespe@ucdavis.edu>
2020-05-15 22:14:05 -0600

    Update docs.

---
+++ R/compile_models.R
@@ -0,0 +1,18 @@
+##' This compiles the models needed for the package. 
+##'
+##' Compile models.
+##' @title Compile models
+##' @param model_names the names of the models to compile
+##' @param models full file path to the location of models
+##' @return NULL
+##' @author Matt Espe
+##' @export
+compile_models = function(model_names = c("eDNA_omni.stan", "eDNA_sim_omni.stan"),
+                          models = system.file("stan_files", model_names,
+                                               package = "artemis"))
+{
+    m = lapply(models, cmdstan_model)
+    return(invisible(NULL))
+    
+}
+
