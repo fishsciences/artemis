b83681b90e7d58d52498838c1f4161ee6a397b32
Matt Espe <mespe@ucdavis.edu>
2020-08-26 18:50:45 -0600

    Put the rstan sampling method back in.

--- R/sim_funs.R
+++ R/sim_funs.R
@@ -27,14 +27,10 @@ sim_eDNA_lm = function(formula, variable_list,
     md = prep_data.sim(ml, std_curve_alpha, std_curve_beta, sigma_Cq, betas,
                        prob_zero, prior_int = normal(), prior_b = normal())
 
-    # if(!verbose) sink(sink_file)
-    model = cmdstan_model(system.file("stan_files","eDNA_sim_omni.stan",
-                                      package = "artemis"))
-    sm = model$sample(data = md, chains = 1L, iter_sampling = n_sim,
-                        fixed_param = TRUE, iter_warmup = 0L)
-
-    # if(!verbose) sink()
-    sims = read_stan_csv(sm$output_files())
+    sims = sampling(stanmodels$eDNA_sim_omni, data = md, chains = 1L,
+                    algorithm = "Fixed_param", iter = n_sim, warmup = 0L,
+                    refresh = ifelse(verbose, 100, -1), show_messages = verbose,
+                    open_progress = FALSE)
     # hacky
     sims = as(sims, "eDNA_simulation_lm")
     sims = load_slots(sims)
@@ -193,15 +189,10 @@ sim_eDNA_lmer = function(formula, variable_list,
                    betas = betas, prob_zero, rand_sd = sigma_rand,
                    prior_int = normal(), prior_b = normal())
 
-    # if(!verbose) sink(sink_file)
-    model = cmdstan_model(system.file("stan_files","eDNA_sim_omni.stan",
-                                      package = "artemis"))
-    sm = model$sample(data = md, chains = 1L, iter_sampling = n_sim,
-                        fixed_param = TRUE, iter_warmup = 0L)
-
-    # if(!verbose) sink()
-    sims = read_stan_csv(sm$output_files())
-        
+    sims = sampling(stanmodels$eDNA_sim_omni, data = md, chains = 1L,
+                    algorithm = "Fixed_param", iter = n_sim, warmup = 0L,
+                    refresh = ifelse(verbose, 100, -1), show_messages = verbose,
+                    open_progress = FALSE)        
     # hacky
     sims = as(sims, "eDNA_simulation_lmer")
     sims = load_slots(sims)
