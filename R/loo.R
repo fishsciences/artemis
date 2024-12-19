07a0d63eddb3699786c64f8a82dc26a2a6fff5af
Matt Espe <mespe@ucdavis.edu>
2024-12-19 13:05:27 -0700

    Update loo to use cmdstanr's built in.

--- R/loo.R
+++ R/loo.R
@@ -4,24 +4,15 @@
 ##' See ?loo::loo for more information about this calculation
 ##' @title Approximate leave-one-out cross-validation
 ##' @param x a eDNA_model object
-##' @param ... additional args passed to \code{loo}
+##' @param ... additional args passed to \code{$loo()}
 ##' @return a list with the results of the calculation. 
 ##' @author Matt Espe
 ##' @export
 loo.eDNA_model = function(x,
-                          pars = "log_lik",
-                          ...,
-                          save_psis = FALSE,
-                          cores = getOption("mc.cores", 1))
+                          ...)
 ## Based on LOO documentation https://mc-stan.org/loo/reference/loo.html
 {
-  stopifnot(length(pars) == 1L)
-  LLarray <- x@fit$draws(variables = pars)
-  r_eff <- loo::relative_eff(x = exp(LLarray), cores = cores)
-  loo::loo.array(LLarray,
-                 r_eff = r_eff,
-                 cores = cores,
-                 save_psis = save_psis)
+  x@fit$loo(...)
 }
 
 
