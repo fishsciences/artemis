566ebdcaea32636256900111e6280e0ef23eacb9
Matt Espe <mespe@ucdavis.edu>
2024-12-19 12:38:21 -0700

    Fix the loo method for using cmdstanr output.

--- R/loo.R
+++ R/loo.R
@@ -9,9 +9,21 @@
 ##' @return a list with the results of the calculation. 
 ##' @author Matt Espe
 ##' @export
-loo.eDNA_model = function(x, ...) {
-              loo(extract_log_lik(x@stanfit), ...)
-          }
+loo.eDNA_model = function(x,
+                          pars = "log_lik",
+                          ...,
+                          save_psis = FALSE,
+                          cores = getOption("mc.cores", 1))
+## Based on LOO documentation https://mc-stan.org/loo/reference/loo.html
+{
+  stopifnot(length(pars) == 1L)
+  LLarray <- x@stanfit$draws(variables = pars)
+  r_eff <- loo::relative_eff(x = exp(LLarray), cores = cores)
+  loo::loo.array(LLarray,
+                 r_eff = r_eff,
+                 cores = cores,
+                 save_psis = save_psis)
+}
 
 
 ##' A 'waic' method for eDNA_model objects, which is simply a wrapper
